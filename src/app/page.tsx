be2b8a40cc492d6adc108eb6c65b7851a83f7786
Sikeee <gautamadhikari911@gmail.com>
2026-09-08 11:22:15 +0530

    Adjust logo size and modify padding for hero section in LandingPage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -79,7 +79,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                   alt="Continental Furnaces"
                   width={180}
                   height={55}
-                  className="h-10 md:h-14 w-auto object-contain"
+                  className="h-12 md:h-20 w-auto object-contain"
                   priority
                 />
               </Link>
@@ -97,9 +97,9 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           </div>
         </nav>
 
-        <main className="pt-[64px] md:pt-[96px] pb-24 lg:pb-0 w-full">
+        <main className="pt-[64px] md:pt-[64px] pb-24 lg:pb-0 w-full">
           {/* HERO SECTION */}
-          <section className="relative overflow-hidden bg-background py-20 lg:py-28 w-full">
+          <section className="relative overflow-hidden bg-background py-12 lg:py-20 w-full">
             {/* Background Image */}
             <div className="absolute inset-0">
               <Image
